4029b28ecd5fbc4b070d4c057b4390ed3a40640a
Mjax <matt.d.jax@gmail.com>
2018-03-06 23:05:52 -0800

    initialized server file

---
+++ Journals/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+//const express = require('express')
+//import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import Notes from './NoteData';
+
+const server = express();
