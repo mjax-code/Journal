5e55b7f856d1fc0476521a7e951fe0aabeff374c
Mjax <matt.d.jax@gmail.com>
2018-03-08 17:31:38 -0500

    Note skeleton

--- Journals/components/Note.js
+++ Journals/components/Note.js
@@ -1,3 +1,15 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
 import styles from '../styleSheets/NoteStyles';
+
+export default class Note extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NoteTitle</Text>
+        <Text>NoteBody</Text>
+        <TextInput/>
+      </View>
+    );
+  }
+}
