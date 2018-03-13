34fc26acd3b72b31ecb644ba56f6456c58cb587b
mjax-code <mjax.code@gmail.com>
2018-03-13 01:48:25 -0400

    updated server and dependencies

--- Journals/server.js
+++ Journals/server.js
@@ -1,7 +1,22 @@
-import express from 'express';
-//const express = require('express')
-//import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import Notes from './NoteData';
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//import Notes from './NoteData';
 
 const server = express();
+server.use(helmet());
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running...'});
+});
+
+//defaul port is 27017
+mongoose.connect('mongodb://localhost/knapsack').then(res => {
+  console.log('connected to mongodb');
+}).catch(err => {
+  console.log('err');
+});
+const port = process.env.PORT || 3000;
+server.listen(port,() => console.log(`running on port: ${port}`));
